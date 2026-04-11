48be4af1a76fe48e97143adeef4add92e4c49ecd
fuuka223 <putarous@gmail.com>
2026-04-11 10:11:35 +0900

    index, new, create等のアクションを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,35 @@
 class TasksController < ApplicationController
   def index
+    @tasks = current_user.tasks
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content, :priority_level, :is_completed)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_10_075318) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_10_234706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "priority_level"
+    t.boolean "is_completed"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_10_075318) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
