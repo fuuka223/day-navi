4047178ab1dfa0d01e068f705b1ced7c79fb09e8
fuuka223 <putarous@gmail.com>
2026-04-13 11:56:36 +0900

    edit,update,destroyのアクションを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,8 @@
 class TasksController < ApplicationController
+
+  before_action :set_task, only: [:edit, :update, :destroy]
+
+
   def index
     @tasks = current_user.tasks
   end
@@ -25,13 +29,25 @@ class TasksController < ApplicationController
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      p @task.errors.full_messages
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
   end
 
   private
   def task_params
     params.require(:task).permit(:title, :content, :priority_level, :is_completed)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
