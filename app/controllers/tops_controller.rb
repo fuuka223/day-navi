3cfcc8842feff65bbb9ba7569b1abf8dc543c18f
fuuka223 <putarous@gmail.com>
2026-04-14 09:05:57 +0900

    トップ画面に今日の予定を追加

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,6 +2,7 @@ class TopsController < ApplicationController
   def index
     if user_signed_in?
       @tasks = current_user.tasks.order(created_at: :desc).limit(5)
+      @today_schedules = current_user.schedules.where(start_time: Time.zone.now.all_day).order(:start_time)
     end
   end
 end
