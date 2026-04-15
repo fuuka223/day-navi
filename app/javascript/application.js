264e3750c34e989bdce20b3346a7a51f30f88db2
fuuka223 <putarous@gmail.com>
2026-04-15 15:05:48 +0900

    タスク一覧のレイアウト

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "./task_form_handler"
+import "./task_list_handler"
