e3439076c6298f0b390d3a9d55cb78f8c82b1c93
fuuka223 <putarous@gmail.com>
2026-04-21 15:31:34 +0900

    Locationモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_16_053422) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_21_061602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.string "ward"
+    t.string "town"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "schedules", force: :cascade do |t|
     t.string "title"
---
+++ spec/factories/locations.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :location do
+    city { "MyString" }
+    ward { "MyString" }
+    town { "MyString" }
+    latitude { 1.5 }
+    longitude { 1.5 }
+  end
+end
