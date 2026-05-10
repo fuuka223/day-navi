87e8e658d2db567de0eac4be8b28b5e9866b8779
fuuka223 <putarous@gmail.com>
2026-05-11 04:41:38 +0900

    本番環境でlocationデータを扱えるように修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,32 +10,42 @@
 
 require 'csv'
 
-puts "福岡県の市区町村代表地点を抽出してインポートを開始します..."
+# puts "福岡県の市区町村代表地点を抽出してインポートを開始します..."
 
-csv_path = Rails.root.join('db', 'csv', 'fukuoka_locations.csv')
+# csv_path = Rails.root.join('db', 'csv', 'fukuoka_locations.csv')
 
-registered_cities = []
-count = 0
+# registered_cities = []
+# count = 0
 
-CSV.foreach(csv_path, headers: false) do |row|
-  city_name = row[1]
-  lat       = row[8]
-  lon       = row[9]
+# CSV.foreach(csv_path, headers: false) do |row|
+#   city_name = row[1]
+#   lat       = row[8]
+#   lon       = row[9]
 
-  if city_name.present? && !registered_cities.include?(city_name)
-    Location.create!(
-      city: city_name,
-      latitude: lat,
-      longitude: lon
-    )
+#   if city_name.present? && !registered_cities.include?(city_name)
+#     Location.create!(
+#       city: city_name,
+#       latitude: lat,
+#       longitude: lon
+#     )
     
-    registered_cities << city_name
-    count += 1
-    puts "#{count}件目: #{city_name} を登録しました"
-  end
+#     registered_cities << city_name
+#     count += 1
+#     puts "#{count}件目: #{city_name} を登録しました"
+#   end
+# end
+
+# puts "----------------------------------------"
+# puts "インポート完了！"
+# puts "福岡県内の #{count} 市区町村を登録しました。"
+# puts "----------------------------------------"
+
+
+
+Location.destroy_all
+
+CSV.foreach(Rails.root.join('db/locations.csv'), headers: true) do |row|
+  Location.create!(row.to_hash)
 end
 
-puts "----------------------------------------"
-puts "インポート完了！"
-puts "福岡県内の #{count} 市区町村を登録しました。"
-puts "----------------------------------------"
+puts "福岡県の市区町村データを #{Location.count} 件登録しました！"
